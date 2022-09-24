ae0a9f800a75c6899dac84790b741106b2fd01e6
Dficci <danielficci@gmail.com>
2022-09-24 09:54:52 -0500

    new repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemonL, setPokemonL] = useState([]);
+  const [savePokemonL, setSavedPokemonL] = useState([]);
+  const [currentPageUrl, SetCurrentPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon"
+  );
+  const [nextPageUrl, setNextPageUrl] = useState();
+  const [prevPageUrl, setPrevPageUrl] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    let cancel;
+    axios
+      .get(currentPageUrl, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setLoading(false);
+        setNextPageUrl(res.data.next);
+        setPrevPageUrl(res.data.previous);
+        setPokemon(res.data.results.map((p) => p.name));
+        setPokemonL(res.data.results.map((p)=>p.url))
+      })
+    return () => cancel();
+
+  }, [currentPageUrl]);
+
+ 
+  useEffect(()=>{
+    for (let i = 0; i < pokemon.length; i++) {
+      axios.all("https://pokeapi.co/api/v2/pokemon/"+pokemon[i])
+      .then(r=>{
+        setPokemonL(r)
+      })
+    }
+  })
+  console.log(pokemonL);
+
+  function goToNextPage() {
+    SetCurrentPageUrl(nextPageUrl);
+  }
+  function goToPrevPage() {
+    SetCurrentPageUrl(prevPageUrl);
+  }
+
+  if (loading) return "loading...";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <PokemonList pokemon={pokemon} />
+      <Pagination
+        gotoNextPage={nextPageUrl ? goToNextPage : null}
+        gotoPrevPage={prevPageUrl ? goToPrevPage : null}
+      />
+    </>
   );
 }
 
